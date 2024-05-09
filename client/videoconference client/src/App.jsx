e6363814e21a697e8043ae49d2c483292012df09
mueezejaz <140510796+mueezejaz@users.noreply.github.com>
2024-05-09 20:31:06 +0500

    setting up react router in client

--- client/videoconference client/src/App.jsx
+++ client/videoconference client/src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-  return <h1 className="text-3xl font-bold ">Hello world!</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
--- client/videoconference client/tailwind.config.js
+++ client/videoconference client/tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      mainbg: "#070F2B",
+      secondbg: "#1B1A55",
+      thirdbg: "#535C91",
+      fourthbg: "#9290C3",
+    },
     extend: {},
   },
   plugins: [],
