953a3c3a2e6fc865a0217213b2acc03addb0dcef
mueezejaz <140510796+mueezejaz@users.noreply.github.com>
2024-05-09 13:52:53 +0500

    testing husky

--- client/videoconference client/src/App.jsx
+++ client/videoconference client/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
       </p>
     </div>
     <p className="read-the-docs">
-      Click on the Vite and React logos to learn more
+      Click on <table></table>he Vite and React logos to learn more
     </p>
   </>;
 }
